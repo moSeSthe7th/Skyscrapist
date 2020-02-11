c92e83fb56cde33c1cddaeb4aa4187138a3f9d13
Mehmet Musa <mmusa@sabanciuniv.edu>
2020-02-11 21:18:47 +0300

    gem

---
+++ Assets/Resources/Scripts/UI/GemRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotator : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(RotateTheGem());
+    }
+
+    IEnumerator RotateTheGem()
+    {
+        while (true)
+        {
+            transform.Rotate(0, 0.5f, 0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
